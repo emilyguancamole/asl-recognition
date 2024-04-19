04ae2a9e227e1f1dd8610a116b555ab6055d3377
emilyguancamole <emilybuguaidi@gmail.com>
2024-04-18 21:17:13 -0400

    whoops

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ from spoter.gaussian_noise import GaussianNoise
 
 
 def get_default_args():
-    ''' Set an argument parser. 
+    ''' Set an argument parser with default arguments for the training script. 
     Names of the arguments defined in the ArgumentParser correspond to the command-line options prefixed with `--` '''
     parser = argparse.ArgumentParser(add_help=False)
 
@@ -36,9 +36,6 @@ def get_default_args():
     # Data
     parser.add_argument("--training_set_path", type=str, default="WLASL_normalized_train_25fps.csv", help="Path to the training dataset CSV file")
     parser.add_argument("--testing_set_path", type=str, default="WLASL100_test_25fps.csv", help="Path to the testing dataset CSV file")
-    parser.add_argument("--experimental_train_split", type=float, default=None,
-                        help="Determines how big a portion of the training set should be employed (intended for the "
-                             "gradually enlarging training set experiment from the paper)")
 
     parser.add_argument("--validation_set", type=str, choices=["from-file", "split-from-train", "none"],
                         default="from-file", help="Type of validation set construction. See README for further rederence")
@@ -129,37 +126,25 @@ def train(args):
     # Training set
     transform = transforms.Compose([GaussianNoise(args.gaussian_mean, args.gaussian_std)])
     train_set = CzechSLRDataset(args.training_set_path, transform=transform, augmentations=True)#, normalize=False)
+    print("type train_set: ", type(train_set))
+    train_loader = DataLoader(train_set, shuffle=True, generator=g)
 
     # Validation set
     if args.validation_set == "from-file":
         val_set = CzechSLRDataset(args.validation_set_path)
         val_loader = DataLoader(val_set, shuffle=True, generator=g)
-
     elif args.validation_set == "split-from-train":
         train_set, val_set = __balance_val_split(train_set, 0.2)
-
         val_set.transform = None
         val_set.augmentations = False
         val_loader = DataLoader(val_set, shuffle=True, generator=g)
-
-    else:
-        val_loader = None
 
     # Testing set
     if args.testing_set_path:
         eval_set = CzechSLRDataset(args.testing_set_path)
         eval_loader = DataLoader(eval_set, shuffle=True, generator=g)
-
-    else:
-        eval_loader = None
-
-    # Final training set refinements
-    if args.experimental_train_split:
-        train_set = __split_of_train_sequence(train_set, args.experimental_train_split)
-
-    print("type train_set: ", type(train_set))
-    train_loader = DataLoader(train_set, shuffle=True, generator=g)
-
+    # else:
+    #     eval_loader = None
 
     # MARK: TRAINING
     train_acc, val_acc = 0, 0
@@ -168,24 +153,18 @@ def train(args):
     top_train_acc, top_val_acc = 0, 0
     checkpoint_index = 0
 
-    if args.experimental_train_split:
-        print("Starting " + args.experiment_name + "_" + str(args.experimental_train_split).replace(".", "") + "...\n\n")
-        logging.info("Starting " + args.experiment_name + "_" + str(args.experimental_train_split).replace(".", "") + "...\n\n")
-
-    else:
-        print("Starting " + args.experiment_name + "...\n\n")
-        logging.info("Starting " + args.experiment_name + "...\n\n")
+    print("Starting " + args.experiment_name + "...\n\n")
+    logging.info("Starting " + args.experiment_name + "...\n\n")
 
     for epoch in range(args.epochs):
         train_loss, _, _, train_acc = train_epoch(slrt_model, train_loader, cel_criterion, sgd_optimizer, device)
         losses.append(train_loss.item() / len(train_loader))
         train_accs.append(train_acc)
-
-        if val_loader:
-            slrt_model.train(False)
-            _, _, val_acc = evaluate(slrt_model, val_loader, device)
-            slrt_model.train(True)
-            val_accs.append(val_acc)
+        # Validation
+        slrt_model.train(False)
+        _, _, val_acc = evaluate(slrt_model, val_loader, device)
+        slrt_model.train(True)
+        val_accs.append(val_acc)
 
         # Save checkpoints if they are best in the current subset
         if args.save_checkpoints:
@@ -196,14 +175,14 @@ def train(args):
             if val_acc > top_val_acc:
                 top_val_acc = val_acc
                 torch.save(slrt_model, "out-checkpoints/" + args.experiment_name + "/checkpoint_v_" + str(checkpoint_index) + ".pth")
-
+        
+        # Log the progress
         if epoch % args.log_freq == 0:
             print("[" + str(epoch + 1) + "] TRAIN  loss: " + str(train_loss.item() / len(train_loader)) + " acc: " + str(train_acc))
             logging.info("[" + str(epoch + 1) + "] TRAIN  loss: " + str(train_loss.item() / len(train_loader)) + " acc: " + str(train_acc))
-
-            if val_loader:
-                print("[" + str(epoch + 1) + "] VALIDATION  acc: " + str(val_acc))
-                logging.info("[" + str(epoch + 1) + "] VALIDATION  acc: " + str(val_acc))
+            # Validation
+            print("[" + str(epoch + 1) + "] VALIDATION  acc: " + str(val_acc))
+            logging.info("[" + str(epoch + 1) + "] VALIDATION  acc: " + str(val_acc))
 
             print("")
             logging.info("")
@@ -215,7 +194,7 @@ def train(args):
 
         lr_progress.append(sgd_optimizer.param_groups[0]["lr"])
 
-    # MARK: TESTING
+    # MARK: TESTING WITH TEST SET
 
     print("\nTesting checkpointed models starting...\n")
     logging.info("\nTesting checkpointed models starting...\n")
@@ -224,7 +203,7 @@ def train(args):
 
     if eval_loader:
         for i in range(checkpoint_index):
-            for checkpoint_id in ["t", "v"]:
+            for checkpoint_id in ["t", "v"]: # t -> train, v -> validation
                 # tested_model = VisionTransformer(dim=2, mlp_dim=108, num_classes=100, depth=12, heads=8)
                 tested_model = torch.load("out-checkpoints/" + args.experiment_name + "/checkpoint_" + checkpoint_id + "_" + str(i) + ".pth")
                 tested_model.train(False)
@@ -243,34 +222,37 @@ def train(args):
 
     # PLOT 0: Performance (loss, accuracies) chart plotting
     if args.plot_stats:
-        fig, ax = plt.subplots()
-        ax.plot(range(1, len(losses) + 1), losses, c="#D64436", label="Training loss")
-        ax.plot(range(1, len(train_accs) + 1), train_accs, c="#00B09B", label="Training accuracy")
-
-        if val_loader:
-            ax.plot(range(1, len(val_accs) + 1), val_accs, c="#E0A938", label="Validation accuracy")
-
-        ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-
-        ax.set(xlabel="Epoch", ylabel="Accuracy / Loss", title="")
-        plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=4, fancybox=True, shadow=True, fontsize="xx-small")
-        ax.grid()
-
-        fig.savefig("out-img/" + args.experiment_name + "_loss.png")
-
+        plot_performance(losses, train_accs, val_accs)
     # PLOT 1: Learning rate progress
     if args.plot_lr:
-        fig1, ax1 = plt.subplots()
-        ax1.plot(range(1, len(lr_progress) + 1), lr_progress, label="LR")
-        ax1.set(xlabel="Epoch", ylabel="LR", title="")
-        ax1.grid()
-
-        fig1.savefig("out-img/" + args.experiment_name + "_lr.png")
+        plot_learning_rate(lr_progress)
 
     print("\nAny desired statistics have been plotted.\nThe experiment is finished.")
     logging.info("\nAny desired statistics have been plotted.\nThe experiment is finished.")
 
+def plot_performance(losses, train_accs, val_accs):
+    fig, ax = plt.subplots()
+    ax.plot(range(1, len(losses) + 1), losses, c="#D64436", label="Training loss")
+    ax.plot(range(1, len(train_accs) + 1), train_accs, c="#00B09B", label="Training accuracy")
+    
+    ax.plot(range(1, len(val_accs) + 1), val_accs, c="#E0A938", label="Validation accuracy")
 
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    ax.set(xlabel="Epoch", ylabel="Accuracy / Loss", title="")
+    plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=4, fancybox=True, shadow=True, fontsize="xx-small")
+    ax.grid()
+
+    fig.savefig("out-img/" + args.experiment_name + "_loss.png")
+    
+def plot_learning_rate(lr_progress):
+    fig1, ax1 = plt.subplots()
+    ax1.plot(range(1, len(lr_progress) + 1), lr_progress, label="LR")
+    ax1.set(xlabel="Epoch", ylabel="LR", title="")
+    ax1.grid()
+
+    fig1.savefig("out-img/" + args.experiment_name + "_lr.png")
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("", parents=[get_default_args()], add_help=False)
     args = parser.parse_args()
